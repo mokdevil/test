94cbe0109e31d019744b34976dadf071bf7c3e61
mokdevil <122613579+mokdevil@users.noreply.github.com>
2023-01-17 14:57:32 +0200

    Add files via upload

---
+++ execute_file.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from io import StringIO
+
+st.title("File read test")
+st.write("Choose python file to display")
+
+uploaded_file = st.file_uploader("Choose a file", type=["py"])
+
+if uploaded_file is not None:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    string_data = stringio.read()
+    st.code(string_data, language="python")
+    exec(string_data)
